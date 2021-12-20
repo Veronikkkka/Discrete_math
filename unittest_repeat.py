cbad56a169cfa6f5169cb1b0cc66f6e7ca147288
Veronikkkka <91616836+Veronikkkka@users.noreply.github.com>
2021-12-20 23:17:13 +0200

    Delete unittest_repeat.py

--- unittest_repeat.py
+++
@@ -1,8 +0,0 @@
-import unittest
-from repeat import repeat
-class TestRepeat(unittest.TestCase):
-    def test_repeat(self):
-        result = list(next(repeat('ABC')))
-        self.assertEqual(result, ['ABC'])
-if __name__ == 'main':
-    unittest.main()
