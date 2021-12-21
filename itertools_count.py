edf15838799be1bc6a56b7ca243c21aac308a857
Veronikkkka <91616836+Veronikkkka@users.noreply.github.com>
2021-12-21 15:19:09 +0200

    Update itertools_count.py

--- itertools_count.py
+++ itertools_count.py
@@ -7,6 +7,9 @@ def count(start=0, step=1):
     :param step: int
     :return: generator
     """
+    if type(start) != int or type(step) != int:
+        print('Incorrect input, only integers are allowed')
+        exit()
     start_point = start
     while True:
         yield start_point
