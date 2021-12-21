0a67a919e258ab4d0f1b6940653f861f19b7f86c
Veronikkkka <91616836+Veronikkkka@users.noreply.github.com>
2021-12-21 15:17:42 +0200

    Update combinations with replacement Tania

--- combinations_with_replacement (2).py
+++ combinations_with_replacement (2).py
@@ -6,23 +6,27 @@ def combinations_with_replacement(elements, number):
     >>> list(combinations_with_replacement([7, 9], 3))
     [(7, 7, 7), (7, 7, 9), (7, 9, 9), (9, 9, 9)]
     """
-    length = len(elements)
-    cells = [0] * number
-    if length == 0 or number == 0:
-        return
-    lst = []
-    for k in cells:
-        lst.append(elements[k])
-    yield tuple(lst)
-    while True:
-        for i in range(number - 1, -1, -1):
-            if cells[i] != length - 1:
-                break
-        else:
+    if type(elements) not in {tuple, list, str}:
+        raise TypeError('Incorrect input, only strings, tuples or lists are allowed')
+    elif type(number) != int:
+        raise TypeError('Incorrect input, only integers are allowed')
+    else:
+        length = len(elements)
+        cells = [0] * number
+        if length == 0 or number == 0:
             return
-        cells[i:] = [cells[i] + 1] * (number - i)
         lst = []
         for k in cells:
             lst.append(elements[k])
         yield tuple(lst)
-
+        while True:
+            for i in range(number - 1, -1, -1):
+                if cells[i] != length - 1:
+                    break
+            else:    
+                break
+            cells[i:] = [cells[i] + 1] * (number - i)
+            lst = []
+            for k in cells:
+                lst.append(elements[k])
+            yield tuple(lst)
