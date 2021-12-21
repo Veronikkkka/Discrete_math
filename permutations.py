0d4c50c4e1c98c6144fc98b4a05e19ecd69bd78a
Veronikkkka <91616836+Veronikkkka@users.noreply.github.com>
2021-12-21 13:33:58 +0200

    Update

--- permutations.py
+++ permutations.py
@@ -51,16 +51,3 @@ def permutations(iterable, length=None):
                 cycles[idx] = len(iterable) - idx
         else:
             return
-import unittest
-
-class TestCombinations(unittest.TestCase):
-    def test_combinations(self):
-        self.assertEqual(list(permutations('ABCD', 3)), [('A', 'B', 'C'), ('A', 'B', 'D'), ('A', 'C', 'B'), ('A', 'C', 'D'), ('A', 'D', 'B'), ('A', 'D', 'C'), ('B', 'A', 'C'), ('B', 'A', 'D'), ('B', 'C', 'A'), ('B', 'C', 'D'), ('B', 'D', 'A'), ('B', 'D', 'C'), ('C', 'A', 'B'), ('C', 'A', 'D'), ('C', 'B', 'A'), ('C', 'B', 'D'), ('C', 'D', 'A'), ('C', 'D', 'B'), ('D', 'A', 'B'), ('D', 'A', 'C'), ('D', 'B', 'A'), ('D', 'B', 'C'), ('D', 'C', 'A'), ('D', 'C', 'B')])
-    
-    def test_combinations(self):
-        self.assertEqual(list(permutations(['K', 'A', 'T', 'E'], 4)), [('K', 'A', 'T', 'E'), ('K', 'A', 'E', 'T'), ('K', 'T', 'A', 'E'), ('K', 'T', 'E', 'A'), ('K', 'E', 'A', 'T'), ('K', 'E', 'T', 'A'), ('A', 'K', 'T', 'E'), ('A', 'K', 'E', 'T'), ('A', 'T', 'K', 'E'), ('A', 'T', 'E', 'K'), ('A', 'E', 'K', 'T'), ('A', 'E', 'T', 'K'), ('T', 'K', 'A', 'E'), ('T', 'K', 'E', 'A'), ('T', 'A', 'K', 'E'), ('T', 'A', 'E', 'K'), ('T', 'E', 'K', 'A'), ('T', 'E', 'A', 'K'), ('E', 'K', 'A', 'T'), ('E', 'K', 'T', 'A'), ('E', 'A', 'K', 'T'), ('E', 'A', 'T', 'K'), ('E', 'T', 'K', 'A'), ('E', 'T', 'A', 'K')])
-
-    def test_combinations(self):
-        self.assertEqual(list(permutations(['B', 'R', 'E', 'N', 'D', 'I'], 7)), [])
-
-unittest.main()
