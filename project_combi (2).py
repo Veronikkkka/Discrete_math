229989a346ded2162898a2fa3df66b0eb7f4ae76
Veronikkkka <91616836+Veronikkkka@users.noreply.github.com>
2021-12-21 15:10:43 +0200

    Update Kate combinations

--- project_combi (2).py
+++ project_combi (2).py
@@ -1,30 +1,29 @@
 def combinations(r, n):
     """
-    # Returns n length subsequences of elements from the r.
-    #
-    # Parameters:
-    # r: iterable
-    # n: length of subsequence
-    #
-    # Returns:
-    # generator of combinations of the given iterable(r)
-    #
-    # >>> list(combinations('ABCD', 3))
-    # [('A', 'B', 'C'), ('A', 'B', 'D'), ('A', 'C', 'D'), ('B', 'C', 'D')]
-    # >>> list(combinations(['K', 'A', 'T', 'E'], 4))
-    # [('K', 'A', 'T', 'E')]
-    # >>> list(combinations(['B', 'R', 'E', 'N', 'D', 'I'], 7))
-    # []
-    # """
-    if isinstance(r, str) or isinstance(r, list) or isinstance(r, tuple):
-        try:
-            length = len(r)
-            if n == 0:
-                yield tuple()
-            elif n != 0:
-                for idx in range(n - 1, length):
-                    for combi in combinations(r[:idx], n - 1):
-                        yield combi + (r[idx],)
-        except:
-            print("Something is wrong about your input!")
+    Returns n length subsequences of elements from the r.
 
+    Parameters:
+    r: iterable
+    n: length of subsequence
+
+    Returns:
+    generator of combinations of the given iterable(r)
+
+    >>> list(combinations('ABCD', 3))
+    [('A', 'B', 'C'), ('A', 'B', 'D'), ('A', 'C', 'D'), ('B', 'C', 'D')]
+    >>> list(combinations(['K', 'A', 'T', 'E'], 4))
+    [('K', 'A', 'T', 'E')]
+    >>> list(combinations(['B', 'R', 'E', 'N', 'D', 'I'], 7))
+    []
+    """
+    if isinstance(r, str) or isinstance(r, list) or isinstance(r, tuple):
+        length = len(r)
+        if n == 0:
+            yield tuple()
+        elif n != 0:
+            for idx in range(n - 1, length):
+                for combi in combinations(r[:idx], n - 1):
+                    yield combi + (r[idx],)
+    else:
+        print("Something is wrong about your input!")
+        exit()
